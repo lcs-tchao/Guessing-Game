667fcda76f9e6bf1ccf09699918e62d079200c6c
Thomas Chao <Tchao@lcs.on.ca>
2025-01-16 14:47:36 -0500

    Initial Commit

---
+++ Guessing Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guessing Game
+//
+//  Created by Yuk Yeung Chao on 2025-01-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guessing Game/Guessing_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guessing_GameApp.swift
+//  Guessing Game
+//
+//  Created by Yuk Yeung Chao on 2025-01-16.
+//
+
+import SwiftUI
+
+@main
+struct Guessing_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
